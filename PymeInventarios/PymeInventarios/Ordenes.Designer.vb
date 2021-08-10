58bc41f1d0388658b8f34fc91041f9c977657e3d
DanielPCMuller <31355265+DanielPCMuller@users.noreply.github.com>
2021-08-09 19:44:35 -0500

    Ajustes Órdenes

--- PymeInventarios/PymeInventarios/Ordenes.Designer.vb
+++ PymeInventarios/PymeInventarios/Ordenes.Designer.vb
@@ -24,12 +24,9 @@ Partial Class Ordenes
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Ordenes))
         Me.DateTimePicker1 = New System.Windows.Forms.DateTimePicker()
-        Me.Label8 = New System.Windows.Forms.Label()
         Me.Button3 = New System.Windows.Forms.Button()
         Me.Label7 = New System.Windows.Forms.Label()
         Me.Label6 = New System.Windows.Forms.Label()
-        Me.Label5 = New System.Windows.Forms.Label()
-        Me.TXT3 = New System.Windows.Forms.TextBox()
         Me.Label4 = New System.Windows.Forms.Label()
         Me.TXT2 = New System.Windows.Forms.TextBox()
         Me.Button2 = New System.Windows.Forms.Button()
@@ -38,10 +35,22 @@ Partial Class Ordenes
         Me.PictureBox1 = New System.Windows.Forms.PictureBox()
         Me.Button1 = New System.Windows.Forms.Button()
         Me.TXT1 = New System.Windows.Forms.TextBox()
-        Me.TXT4 = New System.Windows.Forms.TextBox()
         Me.TXT5 = New System.Windows.Forms.TextBox()
+        Me.TXT6 = New System.Windows.Forms.TextBox()
         Me.Button4 = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.TXT3 = New System.Windows.Forms.TextBox()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.TXT4 = New System.Windows.Forms.TextBox()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.TXT7 = New System.Windows.Forms.TextBox()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox1.SuspendLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'DateTimePicker1
@@ -49,37 +58,27 @@ Partial Class Ordenes
         Me.DateTimePicker1.CustomFormat = ""
         Me.DateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
         Me.DateTimePicker1.ImeMode = System.Windows.Forms.ImeMode.NoControl
-        Me.DateTimePicker1.Location = New System.Drawing.Point(251, 282)
+        Me.DateTimePicker1.Location = New System.Drawing.Point(127, 139)
         Me.DateTimePicker1.MinDate = New Date(2021, 1, 1, 0, 0, 0, 0)
         Me.DateTimePicker1.Name = "DateTimePicker1"
         Me.DateTimePicker1.Size = New System.Drawing.Size(134, 20)
         Me.DateTimePicker1.TabIndex = 138
         Me.DateTimePicker1.Value = New Date(2021, 7, 17, 0, 0, 0, 0)
         '
-        'Label8
-        '
-        Me.Label8.AutoSize = True
-        Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label8.Location = New System.Drawing.Point(136, 311)
-        Me.Label8.Name = "Label8"
-        Me.Label8.Size = New System.Drawing.Size(84, 13)
-        Me.Label8.TabIndex = 135
-        Me.Label8.Text = "Empresa Pedido"
-        '
         'Button3
         '
-        Me.Button3.Location = New System.Drawing.Point(364, 482)
+        Me.Button3.Location = New System.Drawing.Point(364, 595)
         Me.Button3.Name = "Button3"
         Me.Button3.Size = New System.Drawing.Size(107, 34)
         Me.Button3.TabIndex = 134
-        Me.Button3.Text = "Salir"
+        Me.Button3.Text = "Atrás"
         Me.Button3.UseVisualStyleBackColor = True
         '
         'Label7
         '
         Me.Label7.AutoSize = True
         Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label7.Location = New System.Drawing.Point(135, 447)
+        Me.Label7.Location = New System.Drawing.Point(19, 194)
         Me.Label7.Name = "Label7"
         Me.Label7.Size = New System.Drawing.Size(85, 13)
         Me.Label7.TabIndex = 133
@@ -89,34 +88,17 @@ Partial Class Ordenes
         '
         Me.Label6.AutoSize = True
         Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label6.Location = New System.Drawing.Point(137, 371)
+        Me.Label6.Location = New System.Drawing.Point(20, 168)
         Me.Label6.Name = "Label6"
         Me.Label6.Size = New System.Drawing.Size(49, 13)
         Me.Label6.TabIndex = 131
         Me.Label6.Text = "Cantidad"
         '
-        'Label5
-        '
-        Me.Label5.AutoSize = True
-        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label5.Location = New System.Drawing.Point(135, 213)
-        Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(101, 13)
-        Me.Label5.TabIndex = 129
-        Me.Label5.Text = "Árticulos Solicitados"
-        '
-        'TXT3
-        '
-        Me.TXT3.Location = New System.Drawing.Point(251, 308)
-        Me.TXT3.Name = "TXT3"
-        Me.TXT3.Size = New System.Drawing.Size(134, 20)
-        Me.TXT3.TabIndex = 128
-        '
         'Label4
         '
         Me.Label4.AutoSize = True
         Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label4.Location = New System.Drawing.Point(137, 285)
+        Me.Label4.Location = New System.Drawing.Point(20, 143)
         Me.Label4.Name = "Label4"
         Me.Label4.Size = New System.Drawing.Size(88, 13)
         Me.Label4.TabIndex = 127
@@ -124,19 +106,18 @@ Partial Class Ordenes
         '
         'TXT2
         '
-        Me.TXT2.Location = New System.Drawing.Point(251, 172)
-        Me.TXT2.Multiline = True
+        Me.TXT2.Location = New System.Drawing.Point(127, 60)
         Me.TXT2.Name = "TXT2"
-        Me.TXT2.Size = New System.Drawing.Size(134, 104)
+        Me.TXT2.Size = New System.Drawing.Size(134, 20)
         Me.TXT2.TabIndex = 126
         '
         'Button2
         '
-        Me.Button2.Location = New System.Drawing.Point(138, 482)
+        Me.Button2.Location = New System.Drawing.Point(138, 595)
         Me.Button2.Name = "Button2"
         Me.Button2.Size = New System.Drawing.Size(107, 36)
         Me.Button2.TabIndex = 125
-        Me.Button2.Text = "Borrar"
+        Me.Button2.Text = "Limpiar Campos"
         Me.Button2.UseVisualStyleBackColor = True
         '
         'Label3
@@ -153,10 +134,10 @@ Partial Class Ordenes
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label2.Location = New System.Drawing.Point(135, 149)
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(22, 156)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(93, 13)
+        Me.Label2.Size = New System.Drawing.Size(110, 13)
         Me.Label2.TabIndex = 123
         Me.Label2.Text = "Código del Pedido"
         '
@@ -172,7 +153,8 @@ Partial Class Ordenes
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(25, 482)
+        Me.Button1.Enabled = False
+        Me.Button1.Location = New System.Drawing.Point(25, 595)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(107, 36)
         Me.Button1.TabIndex = 121
@@ -181,52 +163,145 @@ Partial Class Ordenes
         '
         'TXT1
         '
-        Me.TXT1.Location = New System.Drawing.Point(251, 146)
+        Me.TXT1.Location = New System.Drawing.Point(138, 153)
         Me.TXT1.Name = "TXT1"
         Me.TXT1.Size = New System.Drawing.Size(134, 20)
         Me.TXT1.TabIndex = 120
         '
-        'TXT4
-        '
-        Me.TXT4.Location = New System.Drawing.Point(251, 334)
-        Me.TXT4.Multiline = True
-        Me.TXT4.Name = "TXT4"
-        Me.TXT4.Size = New System.Drawing.Size(134, 104)
-        Me.TXT4.TabIndex = 139
-        '
         'TXT5
         '
-        Me.TXT5.Location = New System.Drawing.Point(251, 444)
+        Me.TXT5.Location = New System.Drawing.Point(127, 165)
         Me.TXT5.Name = "TXT5"
         Me.TXT5.Size = New System.Drawing.Size(134, 20)
-        Me.TXT5.TabIndex = 140
+        Me.TXT5.TabIndex = 139
+        '
+        'TXT6
+        '
+        Me.TXT6.Location = New System.Drawing.Point(127, 191)
+        Me.TXT6.Name = "TXT6"
+        Me.TXT6.Size = New System.Drawing.Size(134, 20)
+        Me.TXT6.TabIndex = 140
         '
         'Button4
         '
-        Me.Button4.Location = New System.Drawing.Point(251, 482)
+        Me.Button4.Location = New System.Drawing.Point(251, 595)
         Me.Button4.Name = "Button4"
         Me.Button4.Size = New System.Drawing.Size(107, 34)
         Me.Button4.TabIndex = 141
         Me.Button4.Text = "Consultar Órdenes"
         Me.Button4.UseVisualStyleBackColor = True
         '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Controls.Add(Me.TXT7)
+        Me.GroupBox1.Controls.Add(Me.Button5)
+        Me.GroupBox1.Controls.Add(Me.Label10)
+        Me.GroupBox1.Controls.Add(Me.TXT4)
+        Me.GroupBox1.Controls.Add(Me.Label9)
+        Me.GroupBox1.Controls.Add(Me.TXT6)
+        Me.GroupBox1.Controls.Add(Me.TXT3)
+        Me.GroupBox1.Controls.Add(Me.TXT5)
+        Me.GroupBox1.Controls.Add(Me.Label7)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.DateTimePicker1)
+        Me.GroupBox1.Controls.Add(Me.Label6)
+        Me.GroupBox1.Controls.Add(Me.TXT2)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Location = New System.Drawing.Point(25, 192)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(446, 230)
+        Me.GroupBox1.TabIndex = 142
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Artículos Solicitados"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(19, 63)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(103, 13)
+        Me.Label1.TabIndex = 130
+        Me.Label1.Text = "Código del Producto"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label9.Location = New System.Drawing.Point(19, 89)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(107, 13)
+        Me.Label9.TabIndex = 132
+        Me.Label9.Text = "Nombre del Producto"
+        '
+        'TXT3
+        '
+        Me.TXT3.Location = New System.Drawing.Point(127, 86)
+        Me.TXT3.Name = "TXT3"
+        Me.TXT3.Size = New System.Drawing.Size(134, 20)
+        Me.TXT3.TabIndex = 131
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label10.Location = New System.Drawing.Point(19, 115)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(100, 13)
+        Me.Label10.TabIndex = 134
+        Me.Label10.Text = "Marca del Producto"
+        '
+        'TXT4
+        '
+        Me.TXT4.Location = New System.Drawing.Point(127, 112)
+        Me.TXT4.Name = "TXT4"
+        Me.TXT4.Size = New System.Drawing.Size(134, 20)
+        Me.TXT4.TabIndex = 133
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(25, 428)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(446, 150)
+        Me.DataGridView1.TabIndex = 141
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(320, 173)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(107, 34)
+        Me.Button5.TabIndex = 143
+        Me.Button5.Text = "Agregar Artículo"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(19, 37)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(84, 13)
+        Me.Label5.TabIndex = 145
+        Me.Label5.Text = "Empresa Pedido"
+        '
+        'TXT7
+        '
+        Me.TXT7.Location = New System.Drawing.Point(127, 34)
+        Me.TXT7.Name = "TXT7"
+        Me.TXT7.Size = New System.Drawing.Size(134, 20)
+        Me.TXT7.TabIndex = 144
+        '
         'Ordenes
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(483, 534)
+        Me.ClientSize = New System.Drawing.Size(498, 634)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.GroupBox1)
         Me.Controls.Add(Me.Button4)
-        Me.Controls.Add(Me.TXT5)
-        Me.Controls.Add(Me.TXT4)
-        Me.Controls.Add(Me.DateTimePicker1)
-        Me.Controls.Add(Me.Label8)
         Me.Controls.Add(Me.Button3)
-        Me.Controls.Add(Me.Label7)
-        Me.Controls.Add(Me.Label6)
-        Me.Controls.Add(Me.Label5)
-        Me.Controls.Add(Me.TXT3)
-        Me.Controls.Add(Me.Label4)
-        Me.Controls.Add(Me.TXT2)
         Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.Label2)
@@ -238,18 +313,18 @@ Partial Class Ordenes
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Ordenes"
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents DateTimePicker1 As DateTimePicker
-    Friend WithEvents Label8 As Label
     Friend WithEvents Button3 As Button
     Friend WithEvents Label7 As Label
     Friend WithEvents Label6 As Label
-    Friend WithEvents Label5 As Label
-    Friend WithEvents TXT3 As TextBox
     Friend WithEvents Label4 As Label
     Friend WithEvents TXT2 As TextBox
     Friend WithEvents Button2 As Button
@@ -258,7 +333,17 @@ Partial Class Ordenes
     Friend WithEvents PictureBox1 As PictureBox
     Friend WithEvents Button1 As Button
     Friend WithEvents TXT1 As TextBox
-    Friend WithEvents TXT4 As TextBox
     Friend WithEvents TXT5 As TextBox
+    Friend WithEvents TXT6 As TextBox
     Friend WithEvents Button4 As Button
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Label10 As Label
+    Friend WithEvents TXT4 As TextBox
+    Friend WithEvents Label9 As Label
+    Friend WithEvents TXT3 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Label5 As Label
+    Friend WithEvents TXT7 As TextBox
 End Class
--- PymeInventarios/PymeInventarios/Ordenes.vb
+++ PymeInventarios/PymeInventarios/Ordenes.vb
@@ -2,7 +2,15 @@
 Public Class Ordenes
     Dim Conexion As New MySqlConnection
     Dim Proceso As New MySqlCommand
+    Dim Proceso2 As New MySqlCommand
+    Dim Proceso3 As New MySqlCommand
+    Dim Proceso4 As New MySqlCommand
+    Dim Proceso5 As New MySqlCommand
+    Dim Proceso6 As New MySqlCommand
+    Dim Proceso7 As New MySqlCommand
+
     Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Conexion.Close()
         Close()
         Panel_de_Control.Show()
     End Sub
@@ -13,11 +21,16 @@ Public Class Ordenes
         TXT3.Text = ""
         TXT4.Text = ""
         TXT5.Text = ""
+        TXT6.Text = ""
+        TXT7.Text = ""
+        DataGridView1.Columns.Clear()
+
     End Sub
 
     Private Sub Ordenes_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Try
-            Conexion.ConnectionString = "server=bynejs3dk0uzuzbn2sur-mysql.services.clever-cloud.com; user=ucv0u4lxjvhpcjog; password='hQ8fhikLVvzPAU6RIkpe'; database=bynejs3dk0uzuzbn2sur"
+            Conexion.ConnectionString = "server=remotemysql.com; user=8S2KFbGuCG; password='hJgny67Qbs'; database=8S2KFbGuCG"
+            'Conexion.ConnectionString = "server=bynejs3dk0uzuzbn2sur-mysql.services.clever-cloud.com; user=ucv0u4lxjvhpcjog; password='hQ8fhikLVvzPAU6RIkpe'; database=bynejs3dk0uzuzbn2sur"
             Conexion.Open()
         Catch ex As Exception
             MsgBox("No Se Puede Conectar Con la Base de Datos - No Se Podrá Ingresar Productos")
@@ -29,30 +42,98 @@ Public Class Ordenes
 
         Dim FormatoFecha As Date
         Dim NuevaFecha As Date
+        Dim Precio_Compra As Double
+        Dim ProductosID As String
+        Dim PedidoID As String
+        Dim EmpresaID As String
+        Dim Leer As MySqlDataReader
 
-        If TXT1.Text = "" Or TXT2.Text = "" Or TXT3.Text = "" Or TXT4.Text = "" Or TXT5.Text = "" Then
+        If TXT1.Text = "" Or TXT2.Text = "" Or TXT3.Text = "" Or TXT5.Text = "" Or TXT6.Text = "" Or TXT7.Text = "" Then
             MsgBox("Inserte los Datos de la Orden")
             TXT1.Focus()
-        Else
+
+        ElseIf MsgBox("¿Está Seguro Que Desea Registrar la Órden?", MsgBoxStyle.OkCancel) Then
+
             Try
-                Proceso = New MySqlCommand("Insert Into ordenes(Numero_Pedido, Articulos_Solicitados, Fecha_Pedido, Empresa_Pedido, Cantidad, Precio_Compra)" & Chr(13) & "Values(@Numero_Pedido, @Articulos_Solicitados, @Fecha_Pedido, @Empresa_Pedido, @Cantidad, @Precio_Compra)", Conexion)
-                Proceso.Parameters.AddWithValue("@Numero_Pedido", TXT1.Text)
-                Proceso.Parameters.AddWithValue("@Articulos_Solicitados", TXT2.Text)
-                Proceso.Parameters.AddWithValue("@Fecha_Pedido", DateTimePicker1.Text)
-                Proceso.Parameters.AddWithValue("@Empresa_Pedido", TXT3.Text)
-                Proceso.Parameters.AddWithValue("@Cantidad", TXT4.Text)
-                Proceso.Parameters.AddWithValue("@Precio_Compra", TXT5.Text)
+
+                Proceso2 = New MySqlCommand("INSERT INTO Empresa(Nombre_Empresa)" & Chr(13) & "Values(@Nombre_Empresa)", Conexion)
+                Proceso2.Parameters.AddWithValue("@Nombre_Empresa", TXT7.Text)
+                Proceso2.ExecuteNonQuery()
+                Conexion.Close()
+
+                Conexion.Open()
+                Proceso3 = New MySqlCommand("INSERT INTO Productos(Codigo_Producto, Nombre_Producto, Marca_Producto, Precio_Compra)" & Chr(13) & "Values(@Codigo_Producto, @Nombre_Producto, @Marca_Producto, @Precio_Compra)", Conexion)
+                Proceso3.Parameters.AddWithValue("@Codigo_Producto", TXT2.Text)
+                Proceso3.Parameters.AddWithValue("@Nombre_Producto", TXT3.Text)
+                Proceso3.Parameters.AddWithValue("@Marca_Producto", TXT4.Text)
+                Proceso3.Parameters.AddWithValue("@Precio_Compra", TXT6.Text)
+                Proceso3.ExecuteNonQuery()
+                Conexion.Close()
+
+                Conexion.Open()
+                Proceso = New MySqlCommand("SELECT ID_Empresa FROM Empresa WHERE Nombre_Empresa ='" & TXT7.Text & "'", Conexion)
+                Leer = Proceso.ExecuteReader()
+                If Leer.Read = True Then
+                    EmpresaID = CStr(Leer(0))
+                End If
+                Leer.Close()
+                Proceso.ExecuteNonQuery()
+                Conexion.Close()
+
+                Conexion.Open()
+                Proceso4 = New MySqlCommand("INSERT INTO Orden_Pedido(Numero_Pedido, Fecha_Pedido, Empresa_ID_Empresa)" & Chr(13) & "Values(@Numero_Pedido, @Fecha_Pedido, @Empresa_ID_Empresa)", Conexion)
+                Proceso4.Parameters.AddWithValue("@Numero_Pedido", TXT1.Text)
                 FormatoFecha = DateTimePicker1.Text
                 NuevaFecha = Format(FormatoFecha, "yyyy/MM/dd")
-                Proceso.ExecuteNonQuery()
-                MsgBox("Órden de Compra Registrada y Creada Correctamente")
+                Proceso4.Parameters.AddWithValue("@Fecha_Pedido", NuevaFecha)
+                Proceso4.Parameters.AddWithValue("@Empresa_ID_Empresa", EmpresaID)
+                Proceso4.ExecuteNonQuery()
+                Conexion.Close()
+
+                Conexion.Open()
+                Proceso5 = New MySqlCommand("SELECT ID_Producto FROM Productos WHERE Codigo_Producto ='" & TXT2.Text & "'", Conexion)
+                Leer = Proceso5.ExecuteReader()
+                If Leer.Read = True Then
+                    ProductosID = CStr(Leer(0))
+                End If
+                Leer.Close()
+                Proceso5.ExecuteNonQuery()
+                Conexion.Close()
+
+                Conexion.Open()
+                Proceso6 = New MySqlCommand("SELECT ID_Orden FROM Orden_Pedido WHERE Numero_Pedido ='" & TXT1.Text & "'", Conexion)
+                Leer = Proceso6.ExecuteReader()
+                If Leer.Read = True Then
+                    PedidoID = CStr(Leer(0))
+                End If
+                Leer.Close()
+                Proceso6.ExecuteNonQuery()
+                Conexion.Close()
+
+                Conexion.Open()
+                Proceso7 = New MySqlCommand("INSERT INTO Productos_Pedido(Cantidad, Precio_Compra, Productos_ID_Producto, Orden_Pedido_ID_Orden)" & Chr(13) & "Values(@Cantidad, @Precio_Compra, @Productos_ID_Producto, @Orden_Pedido_ID_Orden)", Conexion)
+                Proceso7.Parameters.AddWithValue("@Cantidad", TXT5.Text)
+                Precio_Compra = TXT5.Text * TXT6.Text
+                Proceso7.Parameters.AddWithValue("@Precio_Compra", Precio_Compra)
+                Proceso7.Parameters.AddWithValue("@Productos_ID_Producto", ProductosID)
+                Proceso7.Parameters.AddWithValue("@Orden_Pedido_ID_Orden", PedidoID)
+                Console.WriteLine(Precio_Compra)
+                Proceso7.ExecuteNonQuery()
+                Conexion.Close()
+
+                MsgBox("Orden Registrada Correctamente")
                 TXT1.Text = ""
                 TXT2.Text = ""
                 TXT3.Text = ""
                 TXT4.Text = ""
                 TXT5.Text = ""
+                TXT6.Text = ""
+                TXT7.Text = ""
                 TXT1.Focus()
-            Catch ex As Exception
+                DataGridView1.Columns.Clear()
+                Button1.Enabled = False
+            Catch ex As MySqlException
+                Console.WriteLine(ex.Message)
                 MsgBox("Error En Registrar y Crear la Orden de Compra")
                 Close()
                 Panel_de_Control.Show()
@@ -65,5 +146,25 @@ Public Class Ordenes
         Consultar_Orden.Show()
     End Sub
 
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        Dim Precio_Compra As Double
 
+        If TXT1.Text = "" Or TXT2.Text = "" Or TXT3.Text = "" Or TXT5.Text = "" Or TXT6.Text = "" Or TXT7.Text = "" Then
+            MsgBox("Inserte los Datos del Producto")
+            TXT1.Focus()
+        Else
+            DataGridView1.Columns.Add("Empresa_Pedido", "Empresa Pedido")
+            DataGridView1.Columns.Add("Código_Producto", "Codigo Producto")
+            DataGridView1.Columns.Add("Nombre_Producto", "Nombre Producto")
+            DataGridView1.Columns.Add("Marca Producto", "Marca Producto")
+            DataGridView1.Columns.Add("Fecha_Pedido", "Fecha Pedido")
+            DataGridView1.Columns.Add("Cantidad", "Cantidad")
+            DataGridView1.Columns.Add("Precio_Compra", "Precio Compra")
+            DataGridView1.Rows.Add(TXT7.Text, TXT2.Text, TXT3.Text, TXT4.Text, DateTimePicker1.Text, TXT5.Text, TXT6.Text)
+            Precio_Compra = TXT5.Text * TXT6.Text
+            Console.WriteLine(Precio_Compra)
+            Button1.Enabled = True
+        End If
+
+    End Sub
 End Class
