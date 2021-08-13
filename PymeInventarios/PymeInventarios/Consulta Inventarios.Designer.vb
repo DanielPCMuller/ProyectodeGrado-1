31965ceb3f6ba95b934e081a1ad16fb692af5af2
Josue <torchadavid@gmail.com>
2021-08-13 02:43:37 +0200

    Se añadió funcionalidad a consultar inventario

--- PymeInventarios/PymeInventarios/Consulta Inventarios.Designer.vb
+++ PymeInventarios/PymeInventarios/Consulta Inventarios.Designer.vb
@@ -26,24 +26,14 @@ Partial Class Consulta_Inventarios
         Me.Label3 = New System.Windows.Forms.Label()
         Me.PictureBox1 = New System.Windows.Forms.PictureBox()
         Me.DataGridView1 = New System.Windows.Forms.DataGridView()
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.TXT1 = New System.Windows.Forms.TextBox()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.Button1 = New System.Windows.Forms.Button()
         Me.Button2 = New System.Windows.Forms.Button()
-        Me.TXT3 = New System.Windows.Forms.TextBox()
         Me.Label4 = New System.Windows.Forms.Label()
         Me.Button4 = New System.Windows.Forms.Button()
         Me.Button3 = New System.Windows.Forms.Button()
         Me.CBO1 = New System.Windows.Forms.ComboBox()
-        Me.Column1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.Column2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.Column3 = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.Column4 = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.Column5 = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.Column6 = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.Column7 = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.Column8 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.CBO2 = New System.Windows.Forms.ComboBox()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
@@ -73,27 +63,10 @@ Partial Class Consulta_Inventarios
         '
         Me.DataGridView1.AllowUserToAddRows = False
         Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Column1, Me.Column2, Me.Column3, Me.Column4, Me.Column5, Me.Column6, Me.Column7, Me.Column8})
         Me.DataGridView1.Location = New System.Drawing.Point(48, 235)
         Me.DataGridView1.Name = "DataGridView1"
         Me.DataGridView1.Size = New System.Drawing.Size(561, 181)
         Me.DataGridView1.TabIndex = 59
-        '
-        'Label1
-        '
-        Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(137, 147)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(110, 13)
-        Me.Label1.TabIndex = 60
-        Me.Label1.Text = "Búsqueda Por Código"
-        '
-        'TXT1
-        '
-        Me.TXT1.Location = New System.Drawing.Point(283, 144)
-        Me.TXT1.Name = "TXT1"
-        Me.TXT1.Size = New System.Drawing.Size(144, 20)
-        Me.TXT1.TabIndex = 61
         '
         'Label2
         '
@@ -121,13 +94,6 @@ Partial Class Consulta_Inventarios
         Me.Button2.TabIndex = 65
         Me.Button2.Text = "Borrar"
         Me.Button2.UseVisualStyleBackColor = True
-        '
-        'TXT3
-        '
-        Me.TXT3.Location = New System.Drawing.Point(283, 196)
-        Me.TXT3.Name = "TXT3"
-        Me.TXT3.Size = New System.Drawing.Size(144, 20)
-        Me.TXT3.TabIndex = 68
         '
         'Label4
         '
@@ -164,69 +130,27 @@ Partial Class Consulta_Inventarios
         Me.CBO1.Size = New System.Drawing.Size(144, 21)
         Me.CBO1.TabIndex = 70
         '
-        'Column1
+        'CBO2
         '
-        Me.Column1.HeaderText = "Codigo Producto"
-        Me.Column1.Name = "Column1"
-        Me.Column1.ReadOnly = True
-        '
-        'Column2
-        '
-        Me.Column2.HeaderText = "Nombre"
-        Me.Column2.Name = "Column2"
-        Me.Column2.ReadOnly = True
-        '
-        'Column3
-        '
-        Me.Column3.HeaderText = "Marca"
-        Me.Column3.Name = "Column3"
-        Me.Column3.ReadOnly = True
-        '
-        'Column4
-        '
-        Me.Column4.HeaderText = "Precio Compra"
-        Me.Column4.Name = "Column4"
-        Me.Column4.ReadOnly = True
-        '
-        'Column5
-        '
-        Me.Column5.HeaderText = "Cantidad"
-        Me.Column5.Name = "Column5"
-        Me.Column5.ReadOnly = True
-        '
-        'Column6
-        '
-        Me.Column6.HeaderText = "Porcentaje"
-        Me.Column6.Name = "Column6"
-        Me.Column6.ReadOnly = True
-        '
-        'Column7
-        '
-        Me.Column7.HeaderText = "Utilidad"
-        Me.Column7.Name = "Column7"
-        Me.Column7.ReadOnly = True
-        '
-        'Column8
-        '
-        Me.Column8.HeaderText = "Precio Venta"
-        Me.Column8.Name = "Column8"
-        Me.Column8.ReadOnly = True
+        Me.CBO2.FormattingEnabled = True
+        Me.CBO2.Location = New System.Drawing.Point(283, 198)
+        Me.CBO2.Name = "CBO2"
+        Me.CBO2.Size = New System.Drawing.Size(144, 21)
+        Me.CBO2.TabIndex = 71
         '
         'Consulta_Inventarios
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(646, 509)
+        Me.Controls.Add(Me.CBO2)
         Me.Controls.Add(Me.CBO1)
         Me.Controls.Add(Me.Button4)
-        Me.Controls.Add(Me.TXT3)
         Me.Controls.Add(Me.Label4)
         Me.Controls.Add(Me.Button3)
         Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.Label2)
-        Me.Controls.Add(Me.TXT1)
-        Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.DataGridView1)
         Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.PictureBox1)
@@ -244,22 +168,12 @@ Partial Class Consulta_Inventarios
     Friend WithEvents Label3 As Label
     Friend WithEvents PictureBox1 As PictureBox
     Friend WithEvents DataGridView1 As DataGridView
-    Friend WithEvents Label1 As Label
-    Friend WithEvents TXT1 As TextBox
     Friend WithEvents Label2 As Label
     Friend WithEvents Button1 As Button
     Friend WithEvents Button2 As Button
-    Friend WithEvents TXT3 As TextBox
     Friend WithEvents Label4 As Label
     Friend WithEvents Button4 As Button
     Friend WithEvents Button3 As Button
     Friend WithEvents CBO1 As ComboBox
-    Friend WithEvents Column1 As DataGridViewTextBoxColumn
-    Friend WithEvents Column2 As DataGridViewTextBoxColumn
-    Friend WithEvents Column3 As DataGridViewTextBoxColumn
-    Friend WithEvents Column4 As DataGridViewTextBoxColumn
-    Friend WithEvents Column5 As DataGridViewTextBoxColumn
-    Friend WithEvents Column6 As DataGridViewTextBoxColumn
-    Friend WithEvents Column7 As DataGridViewTextBoxColumn
-    Friend WithEvents Column8 As DataGridViewTextBoxColumn
+    Friend WithEvents CBO2 As ComboBox
 End Class
--- PymeInventarios/PymeInventarios/Consulta Inventarios.vb
+++ PymeInventarios/PymeInventarios/Consulta Inventarios.vb
@@ -1,12 +1,14 @@
 ﻿Imports MySql.Data.MySqlClient
 Public Class Consulta_Inventarios
     Dim Adaptador As New MySqlDataAdapter
+    Dim Adaptador2 As New MySqlDataAdapter
     Dim Datos As New DataSet
     Dim Conexion As New MySqlConnection
+    Dim Proceso1 As New MySqlCommand
+    Dim Leer As MySqlDataReader
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
-        TXT1.Text = ""
-        CBO1.Text = ""
-        TXT3.Text = ""
+        CBO1.Text = "Seleccione"
+        CBO2.Text = "Seleccione"
     End Sub
 
     Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
@@ -21,7 +23,7 @@ Public Class Consulta_Inventarios
             Conexion.Open()
 
             Dim Consulta As String
-            Consulta = "SELECT Marca_Producto From Productos a JOIN Producto_Inventario b on a.ID_Producto = Productos_ID_Producto"
+            Consulta = "SELECT DISTINCT Marca_Producto From Productos a JOIN Producto_Inventario b on a.ID_Producto = Productos_ID_Producto"
             Adaptador = New MySqlDataAdapter(Consulta, Conexion)
             Datos = New DataSet
             Datos.Tables.Add("Productos")
@@ -29,6 +31,20 @@ Public Class Consulta_Inventarios
             CBO1.DataSource = Datos.Tables("Productos")
             CBO1.ValueMember = "Marca_Producto"
             Conexion.Close()
+            CBO1.Text = "Seleccione"
+
+            Conexion.Open()
+
+            Dim Consulta2 As String
+            Consulta2 = "SELECT DISTINCT Nombre_Producto From Productos a JOIN Producto_Inventario b on a.ID_Producto = Productos_ID_Producto"
+            Adaptador2 = New MySqlDataAdapter(Consulta2, Conexion)
+            Datos = New DataSet
+            Datos.Tables.Add("Productos")
+            Adaptador2.Fill(Datos.Tables("Productos"))
+            CBO2.DataSource = Datos.Tables("Productos")
+            CBO2.ValueMember = "Nombre_Producto"
+            Conexion.Close()
+            CBO2.Text = "Seleccione"
 
         Catch ex As Exception
             MsgBox("No Se Puede Conectar Con la Base de Datos - No Se Podrá Consultar el Inventario")
@@ -40,51 +56,57 @@ Public Class Consulta_Inventarios
         Dim Consulta As String
         Dim Lista As Byte
 
-        If TXT1.Text & CBO1.Text & TXT3.Text = "" Then
+        If CBO1.Text & CBO2.Text = "" Then
             MsgBox("Inserte la Opción de Búsqueda del Producto en Código, Nombre o Marca")
         Else
-            If TXT1.Text <> "" Then
-                Consulta = "Select * From inventarios Where Codigo_Articulo='" & TXT1.Text & "'"
-                Adaptador = New MySqlDataAdapter(Consulta, Conexion)
-                Datos = New DataSet
-                Adaptador.Fill(Datos, "inventarios")
-                Lista = Datos.Tables("inventarios").Rows.Count
-            End If
-
-            If Lista <> 0 Then
-                DataGridView1.DataSource = Datos
-                DataGridView1.DataMember = "inventarios"
-            End If
-
-            If CBO1.Text <> "" Then
-                Consulta = "SELECT Codigo_Producto, Nombre_Producto, Marca_Producto, Precio_Compra 
+            Try
+                Conexion.Open()
+                If CBO1.Text <> "" Then
+                    Consulta = "SELECT a.Codigo_Producto, a.Nombre_Producto, a.Marca_Producto, a.Precio_Compra, 
+                                        b.Cantidad, b.Porcentaje_Utilidad, b.Utilidad, b.Precio_Venta 
                             FROM Productos a JOIN Producto_Inventario b on a.ID_Producto = Productos_ID_Producto 
                             WHERE a.Marca_Producto ='" & CBO1.Text & "'"
+                    'Proceso1 = New MySqlCommand(Consulta, Conexion)
+                    Adaptador = New MySqlDataAdapter(Consulta, Conexion)
+                    Datos = New DataSet
+                    Adaptador.Fill(Datos, "Productos")
+                    Lista = Datos.Tables("Productos").Rows.Count
+                    'Leer = Proceso1.ExecuteReader()
+                    'Leer.Read()
+
+                    'If Leer.HasRows Then
+                    '    While Leer.Read()
+                    '        DataGridView1.Rows.Add(Leer(0).ToString(), Leer(1).ToString(), Leer(2).ToString(), Leer(3).ToString(),
+                    '                               Leer(4).ToString(), Leer(5).ToString(), Leer(6).ToString(), Leer(7).ToString())
+                    '    End While
+                    'Else
+                    '    MsgBox("No se ha encontrado el registro")
+                    'End If
+                End If
+                Conexion.Close()
+                If Lista <> 0 Then
+                    DataGridView1.DataSource = Datos
+                    DataGridView1.DataMember = "Productos"
+                End If
+            Catch ex As MySqlException
+                Console.WriteLine(ex.Message)
+                MsgBox("Error en la Base de Datos")
+            End Try
+
+            If CBO2.Text <> "" Then
+                Consulta = "SELECT a.Codigo_Producto, a.Nombre_Producto, a.Marca_Producto, a.Precio_Compra, 
+                                        b.Cantidad, b.Porcentaje_Utilidad, b.Utilidad, b.Precio_Venta 
+                            FROM Productos a JOIN Producto_Inventario b on a.ID_Producto = Productos_ID_Producto 
+                            WHERE a.Nombre_Producto ='" & CBO2.Text & "'"
                 Adaptador = New MySqlDataAdapter(Consulta, Conexion)
                 Datos = New DataSet
-                'Adaptador.Fill(Datos, "Productos")
-                'Lista = Datos.Tables("Productos").Rows.Count
-                Datos.Tables.Add("tabla")
-                Adaptador.Fill(Datos.Tables("tabla"))
-                DataGridView1.DataSource = Datos
+                Adaptador.Fill(Datos, "Productos")
+                Lista = Datos.Tables("Productos").Rows.Count
             End If
 
             If Lista <> 0 Then
                 DataGridView1.DataSource = Datos
                 DataGridView1.DataMember = "Productos"
-            End If
-
-            If TXT3.Text <> "" Then
-                Consulta = "Select * From inventarios Where Nombre_Articulo='" & TXT3.Text & "'"
-                Adaptador = New MySqlDataAdapter(Consulta, Conexion)
-                Datos = New DataSet
-                Adaptador.Fill(Datos, "inventarios")
-                Lista = Datos.Tables("inventarios").Rows.Count
-            End If
-
-            If Lista <> 0 Then
-                DataGridView1.DataSource = Datos
-                DataGridView1.DataMember = "inventarios"
             End If
         End If
     End Sub
@@ -98,4 +120,23 @@ Public Class Consulta_Inventarios
         Panel_de_Control.Show()
     End Sub
 
+    Private Sub CBO1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles CBO1.SelectedIndexChanged
+        CBO2.Text = ""
+        'If CBO1.Text = "Seleccione" Then
+        '    CBO2.Enabled = True
+        '    CBO2.Text = "Seleccione"
+        'Else
+        '    CBO2.Enabled = False
+        'End If
+    End Sub
+
+    Private Sub CBO2_SelectedIndexChanged(sender As Object, e As EventArgs) Handles CBO2.SelectedIndexChanged
+        CBO1.Text = ""
+        'If CBO2.Text = "" Then
+        '    CBO1.Enabled = True
+        '    CBO2.Text = "Seleccione"
+        'Else
+        '    CBO1.Enabled = False
+        'End If
+    End Sub
 End Class
